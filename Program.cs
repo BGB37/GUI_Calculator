b35c52083453338b9832db7238dc1f76c69f73af
Bekir Gülbey Başoğlu <94611659+BGB37@users.noreply.github.com>
2022-09-22 16:43:02 +0300

    Update Program.cs
    Adjusted the width and heigth of the GUI window. 
    Added a new "." button to let users do some calculations with numbers including dots.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,21 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 using System.Drawing;
-
-
+using System.Security.Cryptography.X509Certificates;
 
 namespace GUI_Calculator
 {
- 
-        class AnaPencere : Form  // For sınıfından türeterek yeni bir sınıf oluşturuyoruz. Çok biçimlilik(Polymorphism) örneği.
-        {
+
+    class AnaPencere : Form  // For sınıfından türeterek yeni bir sınıf oluşturuyoruz. Çok biçimlilik(Polymorphism) örneği.
+    {
         private TextBox txtBox;
-        Button btnZero, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnSub, btnMul, btnEqu,  btnClear, btnDiv;
+        Button btnZero, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnSub, btnMul, btnEqu, btnClear, btnDiv, btnDot;
 
         private string islem = "";
 
         public void btnNumber_click(object dugme, EventArgs e)
         {
-           var t =  ((Button)dugme).Text;  // Burayı incele. Parantez içinde yaptığımız işlem Button'a cast etmek diye bahsediliyor.
+            var t = ((Button)dugme).Text;  // Burayı incele. Parantez içinde yaptığımız işlem Button'a cast etmek diye bahsediliyor.
             txtBox.Text += t; // Atama yapmıyoruz mevcuda ekleme yapıyoruz.
 
             if (txtBox.Text[0] == '0') // İlk girilen sayı 0 sa,
@@ -32,52 +31,97 @@ namespace GUI_Calculator
 
         public void btnIslem_Click(object dugme, EventArgs e)
         {
-            var t = ((Button)dugme).Text;
+            var t = ((Button)dugme).Text; //dugmenin texti dört işlem sembollerinden biri olmak zorunda.
             txtBox.Text = txtBox.Text + " " + t + " ";
 
             islem = t; // Hangi işlem tuşuna basıldığını tutmak için global olarak islem değişkenini string olarak tanımladık. Burada hangi tuşa basıldıysa ona atanmış olan işlem stringini tutacak. X - / - + - -(çıkarma sembolü). 
         }
 
+        public void btnDot_click(object sender, EventArgs e)
+        {
+            txtBox.Text += btnDot.Text;
+        }
+
         public void btnEqu_Click(object dugme, EventArgs e)
         {
+
             var ifade = txtBox.Text.Split(islem[0]); //islem stringinin içindeki ilk elemanı almak için indisli yazdık. Aslında zaten tek karakter ama metot char veri tipi istediği için bu sayede bunu da çözmüş olduk.
-            ulong n1 = Convert.ToUInt64(ifade[0]); // ifadenin solunda kalan sayıya n1 diyoruz.
-            ulong n2 = Convert.ToUInt64(ifade[1]);
-
-            switch(islem)
+            // DOUBLE sayı girilmesi durumuda yapılacak işlemler.
+            if (ifade[0].Contains(".") || ifade[1].Contains("."))
             {
-                case "+":
-                    txtBox.Text = $"{n1 + n2}";
-                    break;
-                case "-":
-                    txtBox.Text = $"{n1 - n2}";
-                    break;
-                case "x":
-                    txtBox.Text = $"{n1 * n2}";
-                    break;
-                case "/":
-                    if(n2 != 0)
-                    {
-                    txtBox.Text = $"{n1 / n2}";
-                    } 
-                    else
-                    {
-                        MessageBox.Show("İkinci sayıyı sıfır girme balım.");
-                        txtBox.Text = txtBox.Text.Split('/')[0];
-                    }
+                double n1 = Convert.ToDouble(ifade[0]);
+                double n2 = Convert.ToDouble(ifade[1]);
 
-                    break;
+                switch (islem)
+                {
+                    case "+":
+                        txtBox.Text = $"{n1 + n2}";
+                        break;
+                    case "-":
+                        txtBox.Text = $"{n1 - n2}";
+                        break;
+                    case "x":
+                        txtBox.Text = $"{n1 * n2}";
+                        break;
+                    case "/":
+                        if (n2 != 0)
+                        {
+                            txtBox.Text = $"{n1 / n2}";
+                        }
+                        else
+                        {
+                            MessageBox.Show("İkinci sayıyı sıfır girme balım.");
+                            txtBox.Text = txtBox.Text.Split('/')[0];
+                        }
+
+                        break;
+                }
+            }
+
+
+            else
+            {
+                ulong n1 = Convert.ToUInt64(ifade[0]); // ifadenin solunda kalan sayıya n1 diyoruz.
+                ulong n2 = Convert.ToUInt64(ifade[1]);
+
+                switch (islem)
+                {
+                    case "+":
+                        txtBox.Text = $"{n1 + n2}";
+                        break;
+                    case "-":
+                        txtBox.Text = $"{n1 - n2}";
+                        break;
+                    case "x":
+                        txtBox.Text = $"{n1 * n2}";
+                        break;
+                    case "/":
+                        if (n2 != 0)
+                        {
+                            txtBox.Text = $"{n1 / n2}";
+                        }
+                        else
+                        {
+                            MessageBox.Show("İkinci sayıyı sıfır girme balım.");
+                            txtBox.Text = txtBox.Text.Split('/')[0];
+                        }
+
+                        break;
+                }
             }
         }
 
-        
+            
+         
+
+
 
         public AnaPencere() // AnaPencerenin constructor'ı.
         {
-           this.Text = "Şablon GUI App";
-           this.BackColor = Color.Wheat;
-           this.Width = 500;
-           this.Height = 500;
+            this.Text = "Şablon GUI App";
+            this.BackColor = Color.Wheat;
+            this.Width = 270;
+            this.Height = 420;
 
             txtBox = new TextBox();
 
@@ -87,8 +131,8 @@ namespace GUI_Calculator
             txtBox.Width = 230;
             txtBox.Font = new Font(new FontFamily("Arial"), 18F);
             txtBox.TextAlign = HorizontalAlignment.Right;
-            
-            btnZero = new Button(); 
+
+            btnZero = new Button();
             btn1 = new Button();
             btn2 = new Button();
             btn3 = new Button();
@@ -117,13 +161,15 @@ namespace GUI_Calculator
             btnDiv = new Button();
             btnEqu = new Button();
             btnClear = new Button();
+            btnDot = new Button();
 
             btnAdd.Click += btnIslem_Click;
             btnSub.Click += btnIslem_Click;
             btnMul.Click += btnIslem_Click;         // Sağ tarafta yazan fonksiyon isimlerini buton isimlerinin Click eventlerine ekliyoruz. En azından benim henüz anladığım bu:)
             btnDiv.Click += btnIslem_Click;
             btnEqu.Click += btnEqu_Click;
-            btnClear.Click += btnClear_click; 
+            btnClear.Click += btnClear_click;
+            btnDot.Click += btnDot_click;
 
             // First Row Start 
             btn9.Text = "9";
@@ -169,12 +215,13 @@ namespace GUI_Calculator
             btn4.Height = 50;
             btn4.Top = 130;
             btn4.Left = 130;
-                
+
             btnAdd.Text = "+";
             btnAdd.Width = 50;
             btnAdd.Height = 50;
             btnAdd.Top = 130;
             btnAdd.Left = 190;
+            btnAdd.Font = new Font(new FontFamily("Arial"), 10F);
             // Second Row End
 
             //Third Row Start
@@ -201,6 +248,7 @@ namespace GUI_Calculator
             btnSub.Height = 50;
             btnSub.Top = 190;
             btnSub.Left = 190;
+            btnSub.Font = new Font(new FontFamily("Arial"), 10F);
             //Third Row End
 
             //Fourth Row Start
@@ -209,37 +257,45 @@ namespace GUI_Calculator
             btnZero.Height = 50;
             btnZero.Top = 250;
             btnZero.Left = 10;
-
+            
             btnMul.Text = "x";
             btnMul.Width = 50;
             btnMul.Height = 50;
             btnMul.Top = 250;
             btnMul.Left = 70;
+            btnMul.Font = new Font(new FontFamily("Arial"), 10F);
 
             btnDiv.Text = "/";
             btnDiv.Width = 50;
             btnDiv.Height = 50;
             btnDiv.Top = 250;
             btnDiv.Left = 130;
+            btnDiv.Font = new Font(new FontFamily("Arial"), 10F);
 
             btnEqu.Text = "=";
-            btnEqu.Width = 50;
+            btnEqu.Width = 230;
             btnEqu.Height = 50;
-            btnEqu.Top = 250;
-            btnEqu.Left = 190;
+            btnEqu.Top = 310;
+            btnEqu.Left = 10;
+            btnEqu.Font = new Font(new FontFamily("Arial"), 10F);
 
-
+            btnDot.Text = ".";
+            btnDot.Width = 50;
+            btnDot.Height = 50;
+            btnDot.Top = 250;
+            btnDot.Left = 190;
+            btnDot.Font = new Font(new FontFamily("Arial"), 20F);
             //Fourth Row End
 
             this.Controls.Add(btnZero);
             this.Controls.Add(btn1);
-            this.Controls.Add(btn2);    
-            this.Controls.Add(btn3);    
+            this.Controls.Add(btn2);
+            this.Controls.Add(btn3);
             this.Controls.Add(btn4);
             this.Controls.Add(btn5);
-            this.Controls.Add(btn6);    
-            this.Controls.Add(btn7);    
-            this.Controls.Add(btn8);    
+            this.Controls.Add(btn6);
+            this.Controls.Add(btn7);
+            this.Controls.Add(btn8);
             this.Controls.Add(btn9);
             this.Controls.Add(btnClear);
             this.Controls.Add(btnAdd);
@@ -248,23 +304,18 @@ namespace GUI_Calculator
             this.Controls.Add(btnDiv);
             this.Controls.Add(btnEqu);
             this.Controls.Add(txtBox);
+            this.Controls.Add(btnDot);
         }
 
-     
+    };
 
-
-        };
-
-
-
-    
     internal class Program
-                {
-                    static void Main()
-                    {
-                        Application.Run(new AnaPencere());
+    {
+        static void Main()
+        {
+            Application.Run(new AnaPencere());
 
-                        
-                    }
-                }
+
+        }
+    }
 }
